37d0b458d148b1c5478428e421117c65a2739ec3
aritendry <ramahafenoaritendryotiniela@gmail.com>
2024-10-29 18:38:57 +0300

    Fin du projet en pyteseract

--- totalize/main.py
+++ totalize/main.py
@@ -1,22 +1,32 @@
 import cv2 as cv
+import re
 import pytesseract
 import pandas as pd
-import re
-from pytesseract import Output
-import numpy as np
 
-# Etape 1 : application de filtre gris sur l'image
+# Étape 1 : Lire l'image
+img = cv.imread("image/ticket_02.png")
 
-chemin = str(input("Entrer le chemin de la photo : "))
+# Étape 2 : Application d'un filtre gris à l'image pour la transformer en niveaux de gris
+gray_filter = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-#E1.a Charger l'image
+# Étape 3 : Application du seuil binaire
+_, thresholded_img = cv.threshold(gray_filter, 155, 255, cv.THRESH_BINARY)
+# _ indique qu'on ignore la valeur de retour correspondant au seuil utilisé
+# cv.THRESH_BINARY applique un seuil binaire : les pixels > 155 deviennent blancs (255), les autres deviennent noirs (0)
 
-img = cv.imread(chemin)
-grayscale = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
+# Étape 4 : Récupération des dimensions de l'image (hauteur et largeur)
+height, width = thresholded_img.shape[:2]
+# [:2] permet d'obtenir uniquement la hauteur et la largeur
 
-#E1.b Afficher l'image en gris
+# Étape 5 : Découpage de l'image en trois colonnes de largeur égale
+noms_img = thresholded_img[0:height, 0:int(width/3)]  # Première colonne (nom)
+prix_img = thresholded_img[0:height, int(width/3):int(2*width/3)]  # Deuxième colonne (prix)
+quantites_img = thresholded_img[0:height, int(2*width/3):width]  # Troisième colonne (quantité)
+# Les colonnes sont divisées en trois parties égales, chacune représentant environ 1/3 de la largeur totale
 
-cv.imshow("Gris" , grayscale)
-cv.waitKey(10000)
-
-
+# Étape 6 : Affichage des images découpées pour vérification
+cv.imshow("Nom", noms_img)
+cv.imshow("Prix", prix_img)
+cv.imshow("Quantité", quantites_img)
+cv.waitKey(0)
+cv.destroyAllWindows()
